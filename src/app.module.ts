72986b3b5d29d04fa030e61636b7e8d36425fc35
Hugo Almeida <hugoalmeidahh@gmail.com>
2021-03-22 22:54:14 -0300

    config connection mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://hugoalmeida:M0nt3r3y1638q@cluster0.tj7sm.mongodb.net/MyMongo?retryWrites=true&w=majority'),  
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
